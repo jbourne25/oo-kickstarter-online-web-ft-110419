14ff512538229b0f6521f47f4660f62327e66814
Jeremy Bourne <jbourne25@yahoo.com>
2019-12-15 20:44:11 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,20 +1,20 @@
 class Backer
   
-  @backed_projects = []
+  @@backed_projects = []
   
   attr_accessor :name 
   
   def initialize(name)
     @name = name 
-    @backed_projects = []
+    @@backed_projects = []
   end
   
-  def backed_projects
-    @backed_projects  
+  def self.backed_projects
+    @@backed_projects  
   end
   
   def back_project(project)
-    @backed_projects << Project.new(project)
+    @@backed_projects << Project.new(project)
   end 
   
   
