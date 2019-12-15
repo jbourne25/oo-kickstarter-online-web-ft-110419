915e6db91d89c7f67c011a7e09b14dc4bce1a744
Jeremy Bourne <jbourne25@yahoo.com>
2019-12-15 21:29:33 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -15,7 +15,7 @@ class Backer
   
   def back_project(project)
     #we want all new projects to be added to @backed projects as well
-    @backed_projects << Backer.new(project)
+    @backed_projects << Project 
   end 
   
   
